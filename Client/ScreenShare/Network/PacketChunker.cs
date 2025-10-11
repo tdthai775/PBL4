a4bdfd763d3e363030b4b98de0160a26cd0c6d75
Admin <Admin@DESKTOP-ILTPBDL>
2025-10-11 15:39:56 +0700

    Add project files.

---
+++ Client/ScreenShare/Network/PacketChunker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ScreenShare.Network
+{
+    internal class PacketChunker
+    {
+    }
+}
