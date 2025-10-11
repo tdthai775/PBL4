090216fb323f0858f7e379a151bd96fd6e492640
Thai <Admin@DESKTOP-ILTPBDL>
2025-10-11 23:44:24 +0700

    Encoder

--- Client/ScreenShare/Encoder/FrameResizer.cs
+++ Client/ScreenShare/Encoder/FrameResizer.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Client.ScreenShare.Encoder
 {
+    /// <summary>
+    /// Resize bitmap về kích thước mong muốn
+    /// </summary>
     internal class FrameResizer
     {
+        private readonly int _targetWidth;
+        private readonly int _targetHeight;
+
+        public FrameResizer(int targetWidth, int targetHeight)
+        {
+            _targetWidth = targetWidth;
+            _targetHeight = targetHeight;
+        }
+
+        /// <summary>
+        /// Resize bitmap với chất lượng tốt (bilinear interpolation)
+        /// </summary>
+        public Bitmap Resize(Bitmap original)
+        {
+            // Nếu kích thước giống nhau thì return luôn
+            if (original.Width == _targetWidth && original.Height == _targetHeight)
+            {
+                return new Bitmap(original);
+            }
+
+            var resized = new Bitmap(_targetWidth, _targetHeight);
+
+            using (var graphics = Graphics.FromImage(resized))
+            {
+                // Thiết lập chất lượng resize
+                graphics.InterpolationMode = InterpolationMode.Bilinear;
+                graphics.SmoothingMode = SmoothingMode.HighSpeed;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighSpeed;
+                graphics.CompositingQuality = CompositingQuality.HighSpeed;
+
+                // Vẽ image đã resize
+                graphics.DrawImage(original, 0, 0, _targetWidth, _targetHeight);
+            }
+
+            return resized;
+        }
+
+        /// <summary>
+        /// Resize nhanh hơn nhưng chất lượng thấp hơn
+        /// </summary>
+        public Bitmap ResizeFast(Bitmap original)
+        {
+            if (original.Width == _targetWidth && original.Height == _targetHeight)
+            {
+                return new Bitmap(original);
+            }
+
+            var resized = new Bitmap(_targetWidth, _targetHeight);
+
+            using (var graphics = Graphics.FromImage(resized))
+            {
+                graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                graphics.SmoothingMode = SmoothingMode.HighSpeed;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighSpeed;
+                graphics.CompositingQuality = CompositingQuality.HighSpeed;
+
+                graphics.DrawImage(original, 0, 0, _targetWidth, _targetHeight);
+            }
+
+            return resized;
+        }
     }
 }
--- Client/ScreenShare/Encoder/JpegEncoder.cs
+++ Client/ScreenShare/Encoder/JpegEncoder.cs
@@ -1,12 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Client.ScreenShare.Encoder
 {
+    /// <summary>
+    /// Nén bitmap thành JPEG với chất lượng có thể điều chỉnh
+    /// </summary>
     internal class JpegEncoder
     {
+        private readonly int _quality;
+        private readonly ImageCodecInfo _jpegCodec;
+        private readonly EncoderParameters _encoderParams;
+
+        public JpegEncoder(int quality = 50)
+        {
+            // Quality từ 0-100 (50 = cân bằng giữa chất lượng và kích thước)
+            _quality = Math.Max(0, Math.Min(100, quality));
+
+            // Lấy JPEG codec
+            _jpegCodec = GetJpegCodec();
+
+            // Thiết lập encoder parameters
+            _encoderParams = new EncoderParameters(1);
+            _encoderParams.Param[0] = new EncoderParameter(
+                System.Drawing.Imaging.Encoder.Quality,
+                (long)_quality
+            );
+        }
+
+        /// <summary>
+        /// Encode bitmap thành byte array JPEG
+        /// </summary>
+        public byte[] Encode(Bitmap bitmap)
+        {
+            using (var ms = new MemoryStream())
+            {
+                bitmap.Save(ms, _jpegCodec, _encoderParams);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Encode và ghi ra stream (tiết kiệm memory hơn)
+        /// </summary>
+        public void EncodeToStream(Bitmap bitmap, Stream outputStream)
+        {
+            bitmap.Save(outputStream, _jpegCodec, _encoderParams);
+        }
+
+        /// <summary>
+        /// Lấy kích thước ước lượng sau khi nén
+        /// </summary>
+        public int EstimateSize(Bitmap bitmap)
+        {
+            using (var ms = new MemoryStream())
+            {
+                bitmap.Save(ms, _jpegCodec, _encoderParams);
+                return (int)ms.Length;
+            }
+        }
+
+        /// <summary>
+        /// Tìm JPEG codec
+        /// </summary>
+        private static ImageCodecInfo GetJpegCodec()
+        {
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            var codec = codecs.FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
+
+            if (codec == null)
+            {
+                throw new NotSupportedException("JPEG codec not found on this system.");
+            }
+
+            return codec;
+        }
+
+        public void Dispose()
+        {
+            _encoderParams?.Dispose();
+        }
     }
 }
